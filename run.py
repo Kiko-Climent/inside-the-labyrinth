75779a53bc37774d04321f08ca5faec9d2be8c1a
Kiko Climent <climent.kiko@gmail.com>
2023-10-10 12:49:56 +0000

    First commit, create first two functions and add intro message

--- run.py
+++ run.py
@@ -1,3 +1,30 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+import sys
+
+def intro():
+    """
+    Introcucing the game and request the user's name
+    """
+    print("..you just wake up,\n its dark in here and you realize that you are inside of a Labyrinth")
+    # Intro message
+    # User's name stored in varable 'name'
+    name = input("What's your name?: ")
+    return name
+
+def main():
+    """
+    Main function of the game
+    """
+    name = intro()
+    print(f'\nOk, {name}, how are you gonna get out from here?')
+    ready = input("Are you ready for this? (yes/no): ")
+
+    if ready.lower() == "yes":
+        print("Great! Let's start the adventure.")
+
+    else:
+        print("Definitely, you're not the right person for this.")
+
+if __name__ == "__main__":
+    main()
+
