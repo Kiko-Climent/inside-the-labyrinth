48a3b29b2e338d79dcce98d576fff05f05ed2094
Kiko Climent <climent.kiko@gmail.com>
2023-10-11 19:21:39 +0000

    Add new function to refactor the door logic

--- run.py
+++ run.py
@@ -5,38 +5,28 @@ from colorama import Fore, Style, init, deinit
 init()
 
 def intro():
-    """
-    Introducing the game and request the user's name
-    """
     print("    @@@@@@@@@@@@@@@@@@@@    ")
     print("@@@ INSIDE THE LABYRINTH @@@")
     print("    @@@@@@@@@@@@@@@@@@@@    ")
     print("\n...you just wake up,\n\n its dark in here and you realize that you are inside of a Labyrinth...")
-    """
-    Intro message
-    User's name stored in varable 'name'
-    """
-    name = input("What's your name?: ") # Prompt for user's name
+    name = input("What's your name?: ") 
     return name
 
-def main(): # Main function of the game
-    
+def main():
     name = intro()
     print(f'\nOk, {name}, how are you gonna get out from here?')
-    ready = input("Are you ready for this? (yes/no): ") # Call the first challenge function
+    ready = input("Are you ready for this? (yes/no): ")
 
     if ready.lower() == "yes":
         print("\nGreat! Let's start the adventure.")
-        input("Press ENTER to beggin...")
+        input("Press ENTER to begin...")
         first_challenge(name)
-    
     elif ready.lower() == "no":
         print("\nDefinitely, you're not the right person for this.")
-    
     else:
         print("Wrong answer amigo\n")
 
-def first_challenge(user_name): # Snake and Dragon function
+def first_challenge(user_name):
     print("\nYou find yourself in a room with 2 different doors..")
     print("\nThe first one has a shield with the drawing of a snake hanging over it,")
     print("\nwhile the second one also displays a similar door with a shield featuring a dragon.")
@@ -44,35 +34,48 @@ def first_challenge(user_name): # Snake and Dragon function
 
     if choice.lower() == "snake":
         print("\nYou get into a room full of snakes,\nyou also see a sword hanging on the wall.")
-        snakes_challenge(user_name) # Call the snakes challenge
+        snakes_challenge(user_name)
     elif choice.lower() == "dragon":
         print("\nYou get in a room and you find a huge Dragon")
     else:
         print("\nInvalid choice, please try again")
         choice = input("Which door do you want to open? (snake/dragon): ")
-    
-def snakes_challenge(user_name): #Snake function
+
+def snakes_challenge(user_name):
     print("\nyou can either: ")
     print("\n Take the sword and fight the snakes, ")
     print("\n or run away leaving the sword behind ")
     choice = input("\nWhat do you want to do? (fight/run): ")
 
     if choice.lower() == "fight":
-        print("\n Unfortunately, there are too many snakes and you get bitten. Game over.")
+        print("\n Unfortunately, there are too many snakes and you get bitten. GAME OVER.")
     elif choice.lower() == "run":
         print("\n You leave the sword behind but you also save your life")
-        print("\n You pass a long corridor..\n and you find another door and a magician next to it.")
+        print("\n You pass a long corridor..\nyou find another door and a magician next to it.")
         magician_challenge(user_name)
     else:
         print("\nInvalid choice, please try again")
         choice = input("What do you want to do? (fight/run): ")
 
 
-def magician_challenge(user_name): #Magician function
-    """ 
-    This Function represents the interaction of the user with a magician with multiple options,
-    besides of choosing between different answers, instance the user for a little gambling
-    """
+def door_logic(user_name):
+    print("\nThe Magician steps aside and opens the door for you")
+    print("\n'Have a nice journey' says the Magician")
+    print("\nYou get into another room, there you find 2 different paths\nIt's time to make up your mind again..")
+    choice = input("\nWhere do you want to go? : (left/right): ")
+
+    if choice.lower() == "left":
+        input("\n Press ENTER to continue")
+        print("You took the longest way 'amigo'")
+        input("\n Press again ENTER to continue")
+        print("")
+    else:
+        print("\nInvalid choice, please try again")
+        door_logic(user_name)
+
+def magician_challenge(user_name):
+    talked_to_magician = False
+
     print("What do you want to do now amigo?") 
     print("Talk to the magician or open the door?: ")
     choice = input("(talk/door): ")
@@ -87,7 +90,7 @@ def magician_challenge(user_name): #Magician function
             else :
                 print(f"\nSorry, but {new_name} doesn't match your original name. Try again.")
 
-        print(f"\n ok {new_name}, i guess you dont like the labyrinth, right?")
+        print(f"\n ok {new_name}, I guess you don't like the labyrinth, right?")
         print("\n I can help you to get out from here")
         print("\n Do you want to play a little game with me?")
         choice = input("play/pass: " )
@@ -95,53 +98,31 @@ def magician_challenge(user_name): #Magician function
         if choice.lower() == "play":
             print("\nThe magician gives you a daze\n")
             print("\n 'OK, let's do that'")
-            print(" Throw the daze, if your score is 4 or more i will open a magic door\n wich will bring closer to the exit")
+            print(" Throw the daze, if your score is 4 or more I will open a magic door\n which will bring you closer to the exit")
             print(" But, if you score less than 4,..well, you won't like that young boy")
             input("\nPress ENTER to roll the dice...")
 
             dice_roll = random.randint(1, 6)
-            print(f"\nYou rolles a {dice_roll}!")
+            print(f"\nYou rolled a {dice_roll}!")
 
             if dice_roll >= 4:
                 print(f"\n CONGRATULATIONS {new_name}, let me open that magic door for you")
-                # Magig door needs to be added
+                # Magic door needs to be added
 
             else:
-                print(f"\nOh no! {new_name}, you loose, i hope you like snakes")
+                print(f"\nOh no! {new_name}, you lose, I hope you like snakes")
                 input("\n Press ENTER to continue")
-                print("\nThe Magigian has brought you again to the snake's room")
-                snakes_challenge(user_name) # Not sure about that
+                print("\nThe Magician has brought you again to the snake's room")
+                snakes_challenge(user_name)
 
+        elif choice.lower() == "pass":
+            door_logic(user_name)
 
-        # ALGO FALLA AQUI !! :/:/:/
     elif choice.lower() == "door":
-        print("\nThe Magician steps aside and opens the door for you")
-        print("\n'Have a nice journey' says the Magician")
-        print("\nYou get into another room, there you find 2 different paths\nIt's time to make up your mind again..")
-        choice = input("\nWhere do you want to go? : (left/right): ")
-
-        if choice.lower() == "left":
-            input("\n Press ENTER to continue")
-            print("You took the longest way 'amigo'")
-            input("\n Press again ENTER to continue")
-            print("")
-            
-        
-
+        door_logic(user_name)
 
     else:
-        print("\n Invalid choice, Please try again")
-
-
-    
-
-
-
-
-
-
-
+        print("\nInvalid choice, Please try again")
 
 if __name__ == "__main__":
     main()
-
