6937748c60b163ccbf67dac3a6be6668dade37a9
Kiko Climent <climent.kiko@gmail.com>
2023-10-11 12:18:05 +0000

    Import colorama to add colors in the terminal

--- run.py
+++ run.py
@@ -1,11 +1,14 @@
 import random
 import sys
+from colorama import Fore, Style, init
+
+init()
 
 def intro():
     """
     Introducing the game and request the user's name
     """
-    print("    @@@@@@@@@@@@@@@@@@@@    ")
+    print(f"{Fore.MAGENTA}    @@@@@@@@@@@@@@@@@@@@    ")
     print("@@@ INSIDE THE LABYRINTH @@@")
     print("    @@@@@@@@@@@@@@@@@@@@    ")
     print("...you just wake up,\n its dark in here and you realize that you are inside of a Labyrinth...")
@@ -25,7 +28,7 @@ def main(): # Main function of the game
     if ready.lower() == "yes":
         print("Great! Let's start the adventure.")
         input("Press Enter to beggin...")
-        first_challenge()
+        first_challenge(name)
     
     elif ready.lower() == "no":
         print("Definitely, you're not the right person for this.")
@@ -33,7 +36,7 @@ def main(): # Main function of the game
     else:
         print("Wrong answer amigo\n")
 
-def first_challenge(): # Snake and Dragon function
+def first_challenge(user_name): # Snake and Dragon function
     print("\nYou find yourself in a room with 2 different doors..")
     print("\n The first one has a shield with the drawing of a snake hanging over it,")
     print("\n while the second one also displays a similar door with a shield featuring a dragon.")
@@ -41,14 +44,14 @@ def first_challenge(): # Snake and Dragon function
 
     if choice.lower() == "snake":
         print("\nYou get into a room full of snakes,\nyou also see a sword hanging on the wall.")
-        snakes_challenge()
+        snakes_challenge(user_name)
     elif choice.lower() == "dragon":
         print("\nYou get in a room and you find a huge Dragon")
     else:
         print("\nInvalid choice, please try again")
         choice = input("Which door do you want to open? (snake/dragon): ")
     
-def snakes_challenge(): #Snake function
+def snakes_challenge(user_name): #Snake function
     print("\nyou can either: ")
     print("\n Take the sword and fight the snakes, ")
     print("\n..or run away leaving the sword behind ")
@@ -59,19 +62,35 @@ def snakes_challenge(): #Snake function
     elif choice.lower() == "run":
         print("\n You leave the sword behind but you also save your life")
         print("\n You pass a long corridor..\n and you find another door and a magician next to it.")
-        magician_challenge()
+        magician_challenge(user_name)
     else:
         print("\nInvalid choice, please try again")
         choice = input("What do you want to do? (fight/run): ")
 
-def magician_challenge(): #Magician function
+
+
+
+
+def magician_challenge(user_name): #Magician function
     print("What do you want to do now amigo?") 
     print("Talk to the magician or open the door?: ")
     choice = input("(talk/door): ")
 
     if choice.lower() == "talk":
         print("\n 'Who are you young boy?' asks the magician")
-        
+
+        while True:
+            new_name = input(" ")
+            if new_name.lower() == user_name.lower():
+                break
+            else :
+                print(f"\nSorry, but {new_name} doesn't match your original name. Try again.")
+
+        print(f"\n ok {new_name}, i guess you dont like the labyrinth, right?")
+        print("\n I can help you to get out from here")
+
+
+
 
 
 
